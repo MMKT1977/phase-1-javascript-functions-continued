12e6f5bd7a790bca16e7acd17deabf42d3123648
Kennedy Moranga <kennedy.moranga@student.moringaschool.com>
2022-11-30 18:22:41 +0300

    Failing two tests

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+
+function saturdayFun(funActivity ='roller-skate' ){
+
+       // console.log(`This Saturday, I want to `+ funActivity +`!`);
+    
+        return `This Saturday, I want to `+ funActivity +`!`;
+    }
+saturdayFun();
+saturdayFun('bathe my dog');
+
+const mondayWork = function(result = 'go to the office'){
+    return `This Monday, I will `+ result +`.`;
+};
+mondayWork();
+mondayWork(`work from home`);
+
+function wrapAdjective (){
+    
+     function result(){
+
+  //  return emphatic;
+    }
+    result();
+  //  return "You are"
+
+}
+wrapAdjective();
